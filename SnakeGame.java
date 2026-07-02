ec016c47f268abfbfe3071dc406cc6e6d9006557
Khushi Patel <khushiwonder@gmail.com>
2026-07-02 21:42:54 +0530

    given the direction to the object to move

--- SnakeGame.java
+++ SnakeGame.java
@@ -2,6 +2,15 @@ import processing.core.PApplet;
 
 public class SnakeGame extends PApplet{
   //intial windows setup
+  int snakeX = 300;
+  int snakeY = 200;
+
+  int xSpeed = 0;
+  int ySpeed = 0;
+
+  int foodX = 100;
+  int foodY=100;
+
   public void settings(){
     size(600,400);
   }
@@ -9,21 +18,45 @@ public class SnakeGame extends PApplet{
   public void setup(){
     background(30, 30, 40);
     noStroke();
+    frameRate(8);
   }
 
   public void draw(){
     //container
     fill(30, 30, 40, 20);
     rect(0, 0, width, height);
+    
+    snakeX += xSpeed;
+    snakeY += ySpeed;
 
     //objects
     fill(255,0,0);
-    rect(300,200, 30,30);
-    fill(0, 200, 255); 
-    ellipse(100,100, 25, 25);
-  }
+    rect(snakeX, snakeY, 30,30);
 
+    fill(0, 200, 255); 
+    ellipse(foodX, foodY, 20, 20);
+  }
+  
   public static void main(String[] args){
       PApplet.main("SnakeGame");
+  }
+
+  public void keyPressed(){
+    if(keyCode == RIGHT){
+      xSpeed = 5;
+      ySpeed = 0;
+    }
+    if(keyCode == LEFT){
+      xSpeed = -5;
+      ySpeed = 0;
+    }
+    if(keyCode == UP){
+      xSpeed = 0;
+      ySpeed = -5;
+    }
+    if(keyCode == DOWN){
+      xSpeed = 0;
+      ySpeed = 5;
+    }
   }
 } 
